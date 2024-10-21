e22017bde36518e7d7be23b9611c672aeb51fe71
Mamurik <patymaycer@gmail.com>
2024-10-21 19:35:59 +0300

    Repair UserProgressComponent and moduleCss

--- client/src/pages/Progress/UserLabProgress.tsx
+++ client/src/pages/Progress/UserLabProgress.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { RootState } from "../../store/store";
-import { useGetUserLabProgressQuery } from "../../http/userLabProgressApi";
 import { IUserLabProgress } from "../../API/api";
+import { useGetUserLabProgressQuery } from "../../http/userLabProgressApi";
+import { RootState } from "../../store/store";
 import classes from "./UserLabProgres.module.css";
+import Loader from "../../components/UI/Loader/Loader";
 const UserLabProgress: React.FC = () => {
   const userId = useSelector((state: RootState) => state.user.user?.id);
   const username = useSelector((state: RootState) => state.user.user?.username);
@@ -20,7 +21,7 @@ const UserLabProgress: React.FC = () => {
     );
   }
 
-  if (isLoading) return <div>Загрузка...</div>;
+  if (isLoading) return <Loader></Loader>;
   if (error) {
     let errorMessage = "Неизвестная ошибка";
 
@@ -29,32 +30,42 @@ const UserLabProgress: React.FC = () => {
     } else if ("message" in error) {
       errorMessage = `Ошибка: ${error.message}`;
     }
+    alert(errorMessage);
 
-    return <h1 style={{ textAlign: "center" }}>У пользователя нет лаб</h1>;
+    return (
+      <h1 style={{ textAlign: "center" }}>У пользователя {username} нет лаб</h1>
+    );
   }
 
   return (
-    <div>
-      <h2>Прогресс по лабораторным работам</h2>
+    <div className={classes.container}>
+      <h2 className={classes.container_h2}>
+        Прогресс по лабораторным работам пользователя {username}
+      </h2>
       {userLabProgress && userLabProgress.length > 0 ? (
-        <table>
-          <thead>
-            <tr>
-              <th>Название предмета</th>
-              <th>Номер лабораторной</th>
-              <th>Макс. баллы</th>
-              <th>Статус</th>
+        <table className={classes.progress_table}>
+          <thead className={classes.progress_thead}>
+            <tr className={classes.progress_tr}>
+              <th className={classes.progress_th}>Название предмета</th>
+              <th className={classes.progress_th}>Номер лабораторной</th>
+              <th className={classes.progress_th}>Макс. баллы</th>
+              <th className={classes.progress_th}>Статус</th>
             </tr>
           </thead>
-          <tbody>
+          <tbody className={classes.progress_tbody}>
             {userLabProgress.map((progress: IUserLabProgress) => (
-              <tr key={progress.id}>
-                <td>{progress.lab?.subject?.name || "Без предмета"}</td>
-                <td>{progress.lab?.lab_number || "N/A"}</td>
-                <td>{progress.lab?.max_points || "N/A"}</td>
-                <td>
-                  <input type="range" min="0" max="5" />
-                  {progress.status}/5
+              <tr className={classes.progress_tr} key={progress.id}>
+                <td className={classes.progress_td}>
+                  {progress.lab?.subject?.name || "Без предмета"}
+                </td>
+                <td className={classes.progress_td}>
+                  {progress.lab?.lab_number || "N/A"}
+                </td>
+                <td className={classes.progress_td}>
+                  {progress.lab?.max_points || "N/A"}
+                </td>
+                <td className={classes.progress_td}>
+                  <input type="number" value={progress.status} />
                 </td>
               </tr>
             ))}
